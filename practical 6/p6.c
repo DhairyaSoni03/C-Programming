d6d50d0943c4c7edda833fe93a7689ef790a7a01
DhairyaSoni.CE <dhairyasoni338@gmail.com>
2025-07-29 09:57:55 +0530

    Add files via upload

---
+++ practical 6/p6.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+   float celsius;
+   float fahrenheit;
+    printf("value of celsius is :");
+    scanf("%f",&celsius);
+    fahrenheit=(celsius*9/5)+32;
+    printf("value of temprature in fahrenheit is %f",fahrenheit);
+    return 0;
+}
