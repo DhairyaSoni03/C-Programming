a22f2a84c853e3a3b0f2686fe52fcca343209b5c
DhairyaSoni.CE <dhairyasoni338@gmail.com>
2025-08-06 11:16:23 +0530

    Add files via upload

---
+++ practical 2/p2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ int length=70;
+ int breadth=90;
+ int area;
+ int perimeter;
+ printf("length is 70\n");
+ printf("Breadth is 90\n");
+ area=(length*breadth);
+ printf("Area is %d\n",area);
+ perimeter=2*(length+breadth);
+ printf("perimeter is %d\n",perimeter);
+ return 0;
+}
