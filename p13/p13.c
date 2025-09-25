1d7cd2ed4ebd860659469b25171f2a06218759a4
DhairyaSoni.CE <dhairyasoni338@gmail.com>
2025-09-25 15:53:13 +0530

    Add files via upload

---
+++ p13/p13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int water_level = 0;
+    int max_capacity = 100;
+    int refill_rate = 10;
+
+    while (water_level < max_capacity) {
+        water_level += refill_rate;
+        if (water_level >= max_capacity) {
+            printf("Tank is full.\n");
+            break;
+        }
+        printf("Current water level: %d liters\n", water_level);
+    }
+
+    return 0;
+}
