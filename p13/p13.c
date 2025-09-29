ec7790855b38ea596970f45e4ff664ca4d13f3a9
DhairyaSoni.CE <dhairyasoni338@gmail.com>
2025-09-29 18:46:21 +0530

    p13.c

--- p13/p13.c
+++ p13/p13.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int water_level = 0;
     int max_capacity = 100;
     int refill_rate = 10;
 
-    while (water_level < max_capacity) {
+    while (water_level < max_capacity) 
+    {
         water_level += refill_rate;
-        if (water_level >= max_capacity) {
+        if (water_level >= max_capacity) 
+        {
             printf("Tank is full.\n");
             break;
         }
@@ -16,3 +19,4 @@ int main() {
 
     return 0;
 }
+
