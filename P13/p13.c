5088cf30cce3ba6719a5fc3abe286a04fc54ce7b
DhairyaSoni.CE <dhairyasoni338@gmail.com>
2025-10-07 23:32:18 +1030

    p13.c

--- P13/p13.c
+++ P13/p13.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<windows.h>
 void main()
 {
 	int i;
@@ -6,7 +7,9 @@ void main()
 	for(i=10;i<=100;i+=10)
 	{
 		printf("Current Water Level : %d\n",i);
+		Sleep(3);
 	}
 	printf("Tank is Full.");
 
 }
+
